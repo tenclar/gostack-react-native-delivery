f312465462df0a31c28b7953a57a3b2f93248679
tenclar <tenclar.silva@ac.gov.br>
2020-09-04 15:58:21 -0500

    finalizado

--- src/pages/FoodDetails/index.tsx
+++ src/pages/FoodDetails/index.tsx
@@ -114,11 +114,17 @@ const FoodDetails: React.FC = () => {
 
   function handleIncrementFood(): void {
     // Increment food quantity
+    setFoodQuantity(foodQuantity +1);
 
   }
 
   function handleDecrementFood(): void {
     // Decrement food quantity
+
+
+    if(foodQuantity===1) return;
+
+    setFoodQuantity(foodQuantity -1);
   }
 
   const toggleFavorite = useCallback(() => {
